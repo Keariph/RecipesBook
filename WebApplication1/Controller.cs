0c2fddf8d1de0c2309a5d81d3f9d2d4b5885bbb7
Ryhlinskaya <Ryhlinskaya@bk.ru>
2023-11-03 18:47:27 +0600

    Add a controller with two endpoints.

---
+++ WebApplication1/Controller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace RecipesBook
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+        Repository Repository = new Repository();
+
+        [Route("/")]
+        public ActionResult GetRecipes() 
+        {
+            List<Recipe> recipes = Repository.ReadAll();
+            string response = JsonSerializer.Serialize(recipes);
+            return response == null ? NotFound() : Ok(response);
+        }
+
+        [HttpGet]
+        [Route("/main/recipe/{id}")]
+        public ActionResult PostRecipes([FromHeader] string id) 
+        {
+            Recipe recipe = Repository.ReadOne(id);
+            string response = JsonSerializer.Serialize(recipe);
+            return response == null ? NotFound() : Ok(response);
+        }
+    }
+}
